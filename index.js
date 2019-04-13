44d55d4b5f6415628a37aeb8ee8404dbd8aa840d
joogps <joaogabrielps13@hotmail.com>
2019-04-12 21:46:19 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+let port = process.env.PORT || 8080;
+http.listen(port);
+
+app.get('/', function(req, res) {
+	let pin = req.query.pin;
+	let state = req.query.state;
+
+	if (pin && state)
+		io.sockets.emit('action', {pin: pin, state: state});
+});
+
+io.on('connection', function (socket) {
+	console.log('client connected');
+
+	socket.on('disconnect', function () {
+		io.emit('client disconnected');
+	});
+});
